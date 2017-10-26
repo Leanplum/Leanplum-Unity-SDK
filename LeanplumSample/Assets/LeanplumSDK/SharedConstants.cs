8490c76b37dfd1310ca020e9c63ba9880dbb46b1
Ben Marten <ben.marten@me.com>
2017-10-25 19:36:39 -0700

    Unity SDK 1.5.0

--- LeanplumSample/Assets/LeanplumSDK/SharedConstants.cs
+++ LeanplumSample/Assets/LeanplumSDK/SharedConstants.cs
@@ -24,7 +24,7 @@ namespace LeanplumSDK
 	/// </summary>
 	public class SharedConstants
 	{
-		public const string SDK_VERSION = "1.4.4";
+		public const string SDK_VERSION = "1.5.0";
 
 		public class Kinds
 		{
