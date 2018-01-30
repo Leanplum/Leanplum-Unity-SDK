b5baa9e1124659a6dbea35054f26ed3d44d206a3
Ben Marten <ben.marten@me.com>
2018-01-30 13:41:06 -0800

    build(build.sh): update lp sdk version in gradle template; bumped latest versions for support/build

--- build.sh
+++ build.sh
@@ -195,6 +195,8 @@ main() {
   replace "Android/pom.xml" \
     "<version>%LP_UNITY_VERSION%</version>" "<version>${UNITY_VERSION}</version>"
   replace "LeanplumSample/Assets/Plugins/Android/mainTemplate.gradle" \
+    "%LP_VERSION%" "${ANDROID_SDK_VERSION}"
+  replace "LeanplumSample/Assets/Plugins/Android/mainTemplate.gradle" \
     "%LP_UNITY_VERSION%" "${UNITY_VERSION}"
   replace "Android/pom.xml" "<version>\[1.2.25,)</version>" \
     "<version>${ANDROID_SDK_VERSION}</version>"
