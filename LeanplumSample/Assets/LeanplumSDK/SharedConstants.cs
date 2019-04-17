dbecf220b7259ea1b5b9c49a4e4c9159b0944351
Milos Jakovljevic <milos1290@gmail.com>
2019-04-17 17:10:35 +0200

    Releasing 1.6.7

--- LeanplumSample/Assets/LeanplumSDK/SharedConstants.cs
+++ LeanplumSample/Assets/LeanplumSDK/SharedConstants.cs
@@ -24,7 +24,7 @@ namespace LeanplumSDK
 	/// </summary>
 	public class SharedConstants
 	{
-		public const string SDK_VERSION = "1.7.3";
+		public const string SDK_VERSION = "1.6.7";
 
 		public class Kinds
 		{
