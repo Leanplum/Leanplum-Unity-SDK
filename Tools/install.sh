1d4ceee2ba35101ed10acb10cf9748fd904dc5e6
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-05-10 12:04:46 -0400

    add travis

---
+++ Tools/install.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+BASE_URL=https://download.unity3d.com/download_unity
+HASH=b8cbb5de9840
+VERSION=2018.1.1f1
+
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
