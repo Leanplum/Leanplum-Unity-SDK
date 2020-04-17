ab1c4063760bb8e8e05f6e19a02097e3bb850187
milos1290 <milos1290@gmail.com>
2020-04-17 17:00:27 +0200

    Leanplum Unity SDK 1.6.17

--- LeanplumSample/Assets/LeanplumSDK/SharedConstants.cs
+++ LeanplumSample/Assets/LeanplumSDK/SharedConstants.cs
@@ -24,7 +24,7 @@ namespace LeanplumSDK
 	/// </summary>
 	public class SharedConstants
 	{
-		public const string SDK_VERSION = "2.1.0";
+		public const string SDK_VERSION = "1.6.17";
 
 		public class Kinds
 		{
