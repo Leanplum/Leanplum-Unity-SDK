f2ffd51d26bbe24a25a6c66aa97ca25d20ea5496
milos1290 <milos1290@gmail.com>
2020-04-17 16:58:28 +0200

    Preparing for 2.1.0 release

--- LeanplumSample/Assets/LeanplumSDK/SharedConstants.cs
+++ LeanplumSample/Assets/LeanplumSDK/SharedConstants.cs
@@ -24,7 +24,7 @@ namespace LeanplumSDK
 	/// </summary>
 	public class SharedConstants
 	{
-		public const string SDK_VERSION = "2.0.10";
+		public const string SDK_VERSION = "2.1.0";
 
 		public class Kinds
 		{
