607512f417643e6f7ac2fe1d32897cd2cf9d8f92
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2022-03-15 19:55:22 +0200

    Setup the correct xcframework based on iOS target

--- Leanplum-Unity-SDK/Assets/Editor/LeanplumApplePostProcessor.cs
+++ Leanplum-Unity-SDK/Assets/Editor/LeanplumApplePostProcessor.cs
@@ -3,46 +3,93 @@ using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
+using UnityEditor.iOS.Xcode.Extensions;
+using UnityEngine;
 
 namespace Leanplum.Private
 {
     class LeanplumApplePostProcessor
     {
+        const string FRAMEWORK_NAME = "Leanplum.framework";
+        const string XCFRAMEWORK_NAME = "Leanplum.xcframework";
+        const string DEVICE_SLICE = "ios-arm64_armv7";
+        const string SIMULATOR_SLICE = "ios-arm64_i386_x86_64-simulator";
+        const string DEFAULT_LOCATION_IN_PROJECT = "Frameworks/Plugins/iOS";
+
         [PostProcessBuild]
         public static void OnPostprocessBuild(BuildTarget buildTarget, string path)
         {
             if (buildTarget != BuildTarget.iOS)
                 return;
 
-            const string defaultLocationInProj = "Frameworks/Plugins/iOS";
-            const string fatFrameworkName = "Leanplum.framework";
-            string xcFrameworkPath = "Leanplum.xcframework/ios-arm64_armv7/Leanplum.framework";
-
             iOSSdkVersion target = PlayerSettings.iOS.sdkVersion;
-            if (target == iOSSdkVersion.SimulatorSDK)
-            {
-                xcFrameworkPath = "Leanplum.xcframework/ios-arm64_i386_x86_64-simulator/Leanplum.framework";
-            }
+            SetupXcFramework(path, target == iOSSdkVersion.DeviceSDK);
+        }
 
-            string frameworkPath = Path.Combine(defaultLocationInProj, fatFrameworkName);
+        internal static void SetupXcFramework(string path, bool useDevice = true)
+        {
+            string xcFrameworkDevicePath = Path.Combine(XCFRAMEWORK_NAME, DEVICE_SLICE, FRAMEWORK_NAME);
+            string xcFrameworkSimulatorPath = Path.Combine(XCFRAMEWORK_NAME, SIMULATOR_SLICE, FRAMEWORK_NAME);
 
             string pbxProjectPath = PBXProject.GetPBXProjectPath(path);
             PBXProject project = new PBXProject();
             project.ReadFromFile(pbxProjectPath);
 
             string targetGuid = project.GetUnityMainTargetGuid();
+
+            string frameworkPath = Path.Combine(DEFAULT_LOCATION_IN_PROJECT, xcFrameworkDevicePath);
             string fileGuid = project.FindFileGuidByProjectPath(frameworkPath);
 
-            frameworkPath = Path.Combine(defaultLocationInProj, xcFrameworkPath);
+            string frameworkPathSim = Path.Combine(DEFAULT_LOCATION_IN_PROJECT, xcFrameworkSimulatorPath);
+            string fileGuidSim = project.FindFileGuidByProjectPath(frameworkPathSim);
 
-            if (string.IsNullOrEmpty(fileGuid))
+            if (useDevice)
             {
-                fileGuid = project.FindFileGuidByProjectPath(frameworkPath);
+                if (string.IsNullOrEmpty(fileGuid))
+                {
+                    Debug.Log($"Device {FRAMEWORK_NAME} not found in Xcode project. Path: {frameworkPath}");
+                    return;
+                }
+
+                string xcFrameworkSimulatorDirPath = Path.Combine(path, DEFAULT_LOCATION_IN_PROJECT, XCFRAMEWORK_NAME, SIMULATOR_SLICE);
+
+                Setup(project, fileGuidSim, xcFrameworkSimulatorDirPath, fileGuid);
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(fileGuidSim))
+                {
+                    Debug.Log($"Simulator {FRAMEWORK_NAME} not found in Xcode project. Path: {frameworkPathSim}");
+                    return;
+                }
+
+                string xcFrameworkDeviceDirPath = Path.Combine(path, DEFAULT_LOCATION_IN_PROJECT, XCFRAMEWORK_NAME, DEVICE_SLICE);
+
+                Setup(project, fileGuid, xcFrameworkDeviceDirPath, fileGuidSim);
             }
 
-            UnityEditor.iOS.Xcode.Extensions.PBXProjectExtensions.AddFileToEmbedFrameworks(project, targetGuid, fileGuid);
-
             project.WriteToFile(pbxProjectPath);
+        }
+
+        private static void Setup(PBXProject project, string fileGuidToRemove, string pathToRemove, string fileGuid)
+        {
+            string targetGuid = project.GetUnityMainTargetGuid();
+
+            // Remove simulator/device framework
+            if (!string.IsNullOrEmpty(fileGuidToRemove))
+            {
+                project.RemoveFrameworkFromProject(fileGuidToRemove, FRAMEWORK_NAME);
+                project.RemoveFile(fileGuidToRemove);
+            }
+
+            // Delete simulator/device framework
+            if (Directory.Exists(pathToRemove))
+            {
+                Directory.Delete(pathToRemove, true);
+            }
+
+            // Add device/simulator framework
+            PBXProjectExtensions.AddFileToEmbedFrameworks(project, targetGuid, fileGuid);
         }
     }
 }
