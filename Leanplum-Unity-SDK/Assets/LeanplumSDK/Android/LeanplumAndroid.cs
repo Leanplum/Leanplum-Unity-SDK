5917efd164295e540c6788525699ec51b1d69761
Hristo <hborisoff@gmail.com>
2023-04-21 15:56:48 +0300

    Android: Fix flag for async handlers

--- Leanplum-Unity-SDK/Assets/LeanplumSDK/Android/LeanplumAndroid.cs
+++ Leanplum-Unity-SDK/Assets/LeanplumSDK/Android/LeanplumAndroid.cs
@@ -567,7 +567,7 @@ namespace LeanplumSDK
 
         public override void SetActionManagerUseAsyncHandlers(bool enabled)
         {
-            NativeSDK.CallStatic("useWorkerThreadForDecisionHandlers", true);
+            NativeSDK.CallStatic("useWorkerThreadForDecisionHandlers", enabled);
         }
 
         public override bool ShowMessage(string id)
