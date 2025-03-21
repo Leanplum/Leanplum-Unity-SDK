12c2a83a3e5ed5596bbde13488df9514edb71642
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-03-21 13:03:06 +0200

    Update version and package

--- Leanplum-Unity-SDK/Assets/LeanplumSDK/Utilities/Constants.cs
+++ Leanplum-Unity-SDK/Assets/LeanplumSDK/Utilities/Constants.cs
@@ -24,7 +24,7 @@ namespace LeanplumSDK
     /// </summary>
     public class Constants
     {
-        public const string SDK_VERSION = "6.1.0";
+        public const string SDK_VERSION = "7.0.0";
 
         internal const string CLIENT_PREFIX = "unity";
         internal const string EVENT_EXCEPTION = "__exception";
